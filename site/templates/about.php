3895ab90e4174aa9f312dd056dab6704be8be1f6
trufflesprouts <milkyabdullah@gmail.com>
2017-07-31 10:05:41 +0300

    about update

--- site/templates/about.php
+++ site/templates/about.php
@@ -1,23 +1,51 @@
 <?php snippet('header') ?>
 
-<section class="hero" style="background-image:url(<?= $page->image($page->coverImage()->value())->url() ?>)">
-      <main>
-            <?php snippet('nav') ?>
-            <h1><?= $page->tagline()->html() ?></h1>
-            <div class="intro text">
-              <?= $page->text()->kirbytext() ?>
-            </div>
-      </main>
+<section class="hero-container" style="background-image:url(<?= $page->image($page->coverImage()->value())->url() ?>)">
+  <main>
+    <?php snippet('nav') ?>
+    <div class="hero">
+      <h1><?= $page->tagline()->html() ?></h1>
+      <div class="intro text">
+        <?= $page->text()->kirbytext() ?>
+      </div>
+    </div>
+  </main>
 </section>
+
+
 <main>
-      <section class="services">
-            <h1>What We Do</h1>
-            <?php foreach($page->services()->toStructure() as $servicecategory): ?>
-            <div class="category">
-                  <h2><?= $servicecategory->category()->html() ?></h2>
-                  <p><?= $servicecategory->serviceslist()->markdown() ?></p>
-            </div>
-            <?php endforeach ?>
-      </section>
+
+
+  <section class="services-continer cf">
+    <h1>What We Do</h1>
+    <div class="services">
+      <div class="text">
+        <?php foreach($page->services()->toStructure() as $servicecategory): ?>
+        <div class="category">
+          <h2><?= $servicecategory->category()->html() ?></h2>
+          <p><?= $servicecategory->serviceslist()->markdown() ?></p>
+        </div>
+        <?php endforeach ?>
+      </div>
+      <div class="img" style="background-image:url(<?= $page->image($page->servicesImage()->value())->url() ?>)">
+      </div>
+    </div>
+  </section>
+
+
+  <section class="approach-continer cf">
+    <h1>Our Approach</h1>
+    <div class="blocks">
+      <?php foreach($page->approach()->toStructure() as $block): ?>
+      <div class="block">
+        <img src="<?= $page->image($block->icon()->value())->url() ?>" alt="">
+        <div class="text">
+          <h2><?= $block->title()->html() ?></h2>
+          <p><?= $block->description()->markdown() ?></p>
+        </div>
+      </div>
+      <?php endforeach ?>
+    </div>
+  </section>
 </main>
 <?php snippet('footer') ?>

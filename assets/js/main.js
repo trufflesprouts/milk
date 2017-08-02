e548b2c87f7791c3a2181f48ce69c7281e0d65a0
trufflesprouts <milkyabdullah@gmail.com>
2017-08-02 16:29:43 +0300

    transiton animations

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,3 +6,12 @@ function hamburgerFunction() {
   var hamburger = document.getElementById("hamburger-icon");
   hamburger.classList.toggle('nav-active');
 }
+
+
+function transitionInternal(url) {
+  var transitionCoverDown = document.querySelector('.transition-cover.down');
+  transitionCoverDown.style.display = "block";
+  setTimeout(function () {
+    window.location.href = url;
+  }, 500);
+}
